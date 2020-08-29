833d87188e8e63a20e694c7d444949331c5ae191
unknown <zchen44@syr.edu>
2020-08-29 03:07:06 -0400

    set up deployment to Google App Engine to host Conductor + Basic Auth

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Conductor
 Middleman web server
+
+Hosted on Google App Engine
+Details on console: https://cloud.google.com/sdk/gcloud and https://medium.com/google-cloud/cloud-iot-step-by-step-quality-of-life-tip-the-command-line-ce23046867d4
---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+import json
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+hashes = json.load(open('./.credentials.json'))
+profiles = {
+    "dev": generate_password_hash(hashes['dev']),
+    "prod": generate_password_hash(hashes['prod'])
+}
+
+@app.route("/")
+def homepage():
+    return "Conductor Middleman Server"
+
+@auth.verify_password
+def verify_password(profile, password):
+    if profile in profiles and check_password_hash(profiles.get(profile), password):
+        return profile
+
+@app.route('/credentials/discord', methods=['GET'])
+@auth.login_required
+def discord_bot_token():
+    creds = json.load(open('./.master_cred.json'))
+    data = {'token': creds[auth.username()]['discord_token']}
+    return jsonify(data), 200
+
+if __name__ == '__main__':
+    app.run()
